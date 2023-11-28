3e4d4fab8dea0638860a5a8db25a454d28c64dec
ProgrammingHero1 <mamun2k9kuet@gmail.com>
2023-11-28 22:30:11 +0600

    added payment intent and save payment status to trainers

--- index.js
+++ index.js
@@ -4,11 +4,27 @@ const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyToken = (req, res, next) => {
+  // console.log('inside verify token', req.headers.authorization);
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qfcfzds.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -92,6 +108,41 @@ async function run() {
       res.send(result);
     });
 
+    // payment
+    app.put("/trainers/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+
+        const payment = {
+          status: req.body.status,
+          price: req.body.price,
+        };
+
+        const options = { upsert: true };
+
+        const updateResult = await profileCollection.updateOne(
+          query,
+          {
+            $set: { payment },
+          },
+          options
+        );
+
+        console.log("Server response:", updateResult);
+
+        if (updateResult.modifiedCount > 0) {
+          // Successfully updated the payment status
+          res.sendStatus(200);
+        } else {
+          res.status(404).send("Trainer not found");
+        }
+      } catch (error) {
+        console.error("Error updating payment status:", error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
+
     //class related api
     app.get("/classes", async (req, res) => {
       const result = await classesCollection.find().toArray();
@@ -164,6 +215,21 @@ async function run() {
       );
 
       res.json(updatedPost);
+    });
+
+    // payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
     });
 
     // Send a ping to confirm a successful connection
